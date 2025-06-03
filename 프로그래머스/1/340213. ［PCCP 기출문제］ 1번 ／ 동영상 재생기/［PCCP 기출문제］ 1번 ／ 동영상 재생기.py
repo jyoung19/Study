6f95483b9e15eda1e52b3d046a127b5592e8075f
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2025-06-03 19:01:06 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.06 ms, Memory: 9.29 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,55 +1,33 @@
+def time_to_sec(t):
+    m, s = map(int, t.split(":"))
+    return m * 60 + s
+
+
+def sec_to_time(s):
+    return f"{s // 60:02d}:{s % 60:02d}"
+
+
+def skip_op(pos, op_start, op_end):
+    if op_start <= pos <= op_end:
+        return op_end
+    else:
+        return pos
+
+    
 def solution(video_len, pos, op_start, op_end, commands):
+    video_length = time_to_sec(video_len)
+    pos = time_to_sec(pos)
+    op_start = time_to_sec(op_start)
+    op_end = time_to_sec(op_end)
     
-    video_len_minute = int(video_len.split(':')[0])
-    video_len_second = int(video_len.split(':')[1])
-    video_length = video_len_minute * 60 + video_len_second
-    pos_minute = int(pos.split(':')[0])
-    pos_second = int(pos.split(':')[1])
-    pos_length = pos_minute * 60 + pos_second   
-    
-    
-    # 오프닝 건너뛰기
-    def op(op_start, op_end, pos_length):
-        op_start_minute = int(op_start.split(':')[0])
-        op_start_second = int(op_start.split(':')[1])
-        op_start_length = op_start_minute * 60 + op_start_second
-
-        op_end_minute = int(op_end.split(':')[0])
-        op_end_second = int(op_end.split(':')[1])
-        op_end_length = op_end_minute * 60 + op_end_second   
-
-        if (op_start_length <= pos_length) and (pos_length <= op_end_length):
-            pos_length = op_end_length
-        
-        return pos_length
-    
-    
-    pos_length = op(op_start, op_end, pos_length)
+    pos = skip_op(pos, op_start, op_end)
     
     for command in commands:
-        pos_length = op(op_start, op_end, pos_length)
-            
         # 10초 후로 이동
         if command == "next":
-            if video_length - pos_length < 10:
-                pos_length = video_length
-            else: 
-                pos_length += 10
-        
-            
-        # 10초 전으로 이동
-        if command == "prev":
-            if pos_length < 10:
-                pos_length = 0
-            else: 
-                pos_length -= 10
+            pos = min(video_length, pos + 10)
+        elif command == "prev":
+            pos = max(0, pos - 10)
+        pos = skip_op(pos, op_start, op_end)
     
-    
-    pos_length = op(op_start, op_end, pos_length)
-    
-    ans_minute = pos_length // 60
-    ans_second = pos_length % 60
-    
-    answer = f"{ans_minute:02d}:{ans_second:02d}"
-    
-    return answer
+    return sec_to_time(pos)
