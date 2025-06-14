2ad6a5e651f73654ee4b381a3b0cb1cdc7961961
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2025-06-14 14:41:49 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -1,14 +1,14 @@
+import math
+
 def solution(numer1, denom1, numer2, denom2):
     
     # 분수의 덧셈
     denom3 = denom1 * denom2
     numer3 = numer1 * denom2 + numer2 * denom1  
     
-    # 기약분수로 나타내기
-    for i in range(min(numer3, denom3), 1, -1):
-        if (numer3 % i == 0) and (denom3 % i == 0):
-            numer3 //= i
-            denom3 //= i
-            break
+    # 기약분수로 나타내기 (최대공약수로 나누기)
+    gcd = math.gcd(numer3, denom3)
+    numer3 //= gcd
+    denom3 //= gcd
         
     return (numer3, denom3)
