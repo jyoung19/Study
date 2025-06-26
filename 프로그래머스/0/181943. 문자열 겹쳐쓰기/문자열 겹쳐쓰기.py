3baec407ff6b575cc9f9568f589ac9ff15d22cd6
jyoung19 <98319636+jyoung19@users.noreply.github.com>
2025-06-26 10:00:13 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.00 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,9 +1,3 @@
 def solution(my_string, overwrite_string, s):
-    l1 = len(my_string)
-    l2 = len(overwrite_string)
-    
-    if l1 - l2 == s:
-        answer = my_string[:s] + overwrite_string
-    else:
-        answer = my_string[:s] + overwrite_string + my_string[s+l2:]
+    answer = my_string[:s] + overwrite_string + my_string[s+len(overwrite_string):]
     return answer
